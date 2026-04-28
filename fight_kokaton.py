263cfe2d3eaf2b5264df1ffb30077ea2eeb0311f
c0a250426f <c0a250426f@edu.teu.ac.jp>
2026-04-28 16:13:14 +0900

    エラー解消用

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -142,12 +142,30 @@ class Bomb:
         self.rct.move_ip(self.vx, self.vy)
         screen.blit(self.img, self.rct)
 
+class Score:
+    def __init__(self):
+        self.fonto = pg.font.SysFont(None,30)
+        self.score = 0
+        self.color = (0,0,255)
+        self.img = self.fonto.render(f"Score:{self.score}", 0, self.color)
+        self.rct = self.img.get_rect()
+        self.rct.center = 100,HEIGHT-50
+
+    def update(self, screen: pg.Surface):
+        """
+        スコアを更新して画面に描画する
+        """
+        # 最新スコアの文字列Surfaceを生成
+        self.img = self.fonto.render(f"Score: {self.score}", 0, self.color)
+        # スクリーンに描画
+        screen.blit(self.img, self.rct)
 
 def main():
     pg.display.set_caption("たたかえ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))    
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bird = Bird((300, 200))
+    score = Score()
     #bomb = Bomb((255, 0, 0), 10)
     bombs = [Bomb((255,0,0),10)for i in range(NUM_OF_BOMBS)]
     beam = None  # ゲーム初期化時にはビームは存在しない
@@ -160,7 +178,7 @@ def main():
                 return
             if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
             #スペースキー押下でBeamクラスのインスタンス生成
-                beam = Beam(bird)            
+                beam = Beam(bird)    
         screen.blit(bg_img, [0, 0])
 
         for bomb in bombs:
@@ -180,6 +198,7 @@ def main():
                     beam = None
                     bombs[i] = None
                     bird.change_img(6, screen)
+                    score.score += 1 
                     pg.display.update()
                     time.sleep(1)
 
@@ -191,6 +210,7 @@ def main():
             beam.update(screen)  
         for bomb in bombs: 
             bomb.update(screen)
+        score.update(screen)    
         pg.display.update()
         tmr += 1 
         clock.tick(50)
